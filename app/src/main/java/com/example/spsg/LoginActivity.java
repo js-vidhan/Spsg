cce201f02fb0afd8cf54c4a959c69e3bd416754a
vidhan <js.vidhan@gmail.com>
2024-06-12 20:15:36 +0530

    This is our second commit

--- app/src/main/java/com/example/spsg/LoginActivity.java
+++ app/src/main/java/com/example/spsg/LoginActivity.java
@@ -10,7 +10,7 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private Button loginr;
-//  its just a check for github
+//  its just a check for github project
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
